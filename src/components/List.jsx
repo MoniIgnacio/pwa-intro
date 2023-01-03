32cb105634ff15c8daaa81135acd19af81f88910
MoniIgnacio <moniignacio02@gmail.com>
2023-01-03 22:57:05 +0400

    initial commit

---
+++ src/components/List.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react';
+import useList from '../hooks/useList';
+
+export default function List() {
+  const tasks = useList([]);
+
+  const [newTask, setNewTask] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    tasks.push(newTask);
+    setNewTask('');
+  };
+
+  const handleInputChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Task List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          value={newTask}
+          onChange={handleInputChange}
+          type="text"
+          placeholder="New Task"
+        />
+        <button>Create Task</button>
+      </form>
+      {tasks.isEmpty() ? (
+        <p>Task List is Empty </p>
+      ) : (
+        <ul>
+          {tasks.value.map((task, index) => {
+            return (
+              <li key={index}>
+                <input
+                  type="checkbox"
+                  onClick={() => tasks.remove(index)}
+                  checked={false}
+                />
+                  {task}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </div>
+  );
+}
